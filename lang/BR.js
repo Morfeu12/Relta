e6ba48f57486fceedf36c06b2f87d8f92396e985
Luphia13 <omarcosta152@gmail.com>
2019-10-12 10:45:20 -0300

    Fist Commit

---
+++ lang/BR.js
@@ -0,0 +1,166 @@
+//Variaveis, cada texto tem uma variavel
+var banner_1 = "teste",
+	banner_msg_1 = "teste",
+	banner_2 = "",
+	banner_msg_2 = "",
+	banner_3 = "",
+	banner_msg_3 = "",
+	servico_titulo = "SERVIÇOS PRESTADOS",
+	servico_msg = "Nossos profissionais são capacitados e possuem experiência tanto na área de reformas e construção quanto na área de Legislação, Certificações, Laudos e Gerenciamentos, trabalhando junto aos orgãos competentes de escala municipal à Federal, com o compromisso de satisfazer as necessidades dos nossos clientes e parceiros de maneira rápida e eficaz, sempre pensando na melhoria contínua através de ações preventivas e corretivas adequando todos os serviços e processos dentro dos padrões mais rigorosos de fiscalização e qualidade.",
+	servico_1_titulo = "PREFEITURA",
+	servico_1_desc = "Regularização;<br>Aprovação e Execução de Obras;<br>Aprovação e Execução de Reformas; <br>Licenças de Funcionamento;<br>Desdobros e Remembro de Lotes;<br>Projeto Acessibilidade;<br>Adaptação às Normas de Segurança;<br>Executivo/Construção Nova;<br>CADAN; <br>Tanques e Bombas;<br>Alvará de Equipamentos;<br>Auto de Verificação de Segurança (AVS).",
+	servico_2_titulo = "PROJETOS TÉCNICOS",
+	servico_2_desc = "Decoração e Interiores;<br>Paisagísmo;<br>Projetos Tridimensionais;<br>Comunicação de Pequena Reforma;<br>Laudo Para Condomínio (ABNT 16.280);<br>Laudo de Habitabilidade;<br>Laudo de Viabilidade;<br>As Built.",
+	servico_3_titulo = "CORPO DE BOMBEIROS",
+	servico_3_desc = "Adaptação às Normas do Corpo de Bombeiros;<br>Instalação de Equipamentos;<br>Pressurização de Escadas;<br>AVCB inicial;<br>Renovação;<br>Pré Vistoria;<br>FAT.",
+	port_titulo = "PORTIFÓLIO",
+	port_desc = "Conheça todos os nossos projetos.",
+	port_op_1 = "Casa GP",
+	port_op_2 = "Casa Verde",
+	port_op_3 = "CCSP",
+	port_op_4 = "Guarulhos",
+	port_op_5 = "Vl. Madalena",
+	port_op_6 = "",
+	port_op_1_desc = "Descricao da Obra.",
+	port_op_2_desc = "Descricao da Obra.",
+	port_op_3_desc = "Descricao da Obra.",
+	port_op_4_desc = "Descricao da Obra.",
+	port_op_5_desc = "Descricao da Obra.",
+	port_op_6_desc = "Descricao da Obra.",
+	port_pergunta = "VOCÊ GOSTOU DO NOSSO PORTIFÓLIO?",
+	port_resposta = "Entre em contato agora mesmo e ficaremos satisfeitos em lhe atender.",
+	btn_contato = "Contact Us",
+	sobre_titulo = "QUEM NÓS SOMOS",
+	sobre_subtitulo = "SOBRE NÓS",
+	sobre_msg = "Fundada por Felipe Alves e Raquel Freitas, a Relta Arquitutra desenvolve projetos em diálogo com a natureza e em contato com as devidas intenções, visando todas as necessidades de seus clientes. Felipe e Raquel trabalham juntos desde 2014, mas somente em 2016 ambos decidem fundar a sociedade empresarial.",
+	missao_titulo = "MISSÃO",
+	missao_desc = "Proporcionar soluções ambientais sustentáveis a seus clientes, parceiros e a sociedade, buscando desenvolvimento ambiental equilibrado e consciente.",
+	visao_titulo = "VISÃO",
+	visao_desc = "Ser uma empresa referência em soluções de consultoria, projetos e construções buscando atuar no mercado de forma transparente e ética.",
+	valores_titulo = "VALORES",
+	valores_desc = "Comprometimento, Foco, Objetividade, Praticidade, Sustentabilidade, Ética, Pontualidade, Transparência, Qualidade e Inovação.",
+	equipe_titulo = "A EQUIPE",
+	equipe_desc = "O escritório aposta em uma relação próxima com a natureza, em favor do conceito de verdadeira sustentabilidade. Projetos realizados que trabalham com materiais ecológicos, fazendo amplo uso de elementos como a luminosidade, a transparência, a fluidez dos espaços e a leveza estrutural. Por meio dessa proposta, estabelecendo um conceito de arquitetura brasileira, com a reapropriação do modernismo em diálogo com o meio ambiente.",
+	equipe_p1_nome = "Felipe Alves",
+	equipe_p1_cargo = "Fundador e CEO",
+	equipe_p1_cv = "Ingressou no mercado em 2010, como estagiário em arquitetura, executando ao mesmo tempo projetos em freelance. Em 2011 passou a trabalhar com o arquiteto Roberto Montenegro onde adquiriu experiência com decorações e reformas, posteriormente em 2012 passou a estagiar na empresa Atthelie da Arquitetura, onde adquiriu experiência em Projetos de Aprovações, Cetesb, Covisa, Corpo de Bombeiros, Vigilância Sanitária, Acompanhamento e Gerenciamento de Obras. Em 2014, abriu a empresa como empresário individual, passando a atuar no mercado por conta própria.",
+	equipe_p2_nome = "Raquel Toni",
+	equipe_p2_cargo = "Co-Fundadora e Arquiteta",
+	equipe_p2_cv = "Arquiteta atuante desde 2016, atuou como estagiária no Hospital Israelita Albert Einstein no ano de 2012 realizando projetos de interiores hospitalares, posteriormente no ano de 2013 ingressou como estagiária na empresa Atthelie da Arquitetura, executando análise de processos e projetos junto a Prefeituras. Prestou serviços em 2015 à empresa PW Design - Arquitetura e Interiores, realizando o desenvolvimento de projetos residenciais e comerciais, bem como as definições de mateirais e apresentação de projetos in loco. Além de ter iniciado sua profissão, com dois anos de participação na Prefeitura de São Paulo, para o cadastro fiscal e zoneamento da cidade.",
+	equipe_p3_nome = "Beto Montenegro",
+	equipe_p3_cargo = "Arquiteto",
+	equipe_p3_cv = "Arquiteto atuante, com experiência na área de projeto de arquitetura. Formado pela Faculdade de Arquitetura e Urbanismo da Universidade de São Paulo (FAUUSP, 1998), Mestre (2007) e Doutor (2012) pela mesma Instituição em História da Arquitetura e do Urbanismo, com pesquisa desenvolvida sobre a pré-fabricação na arquitetura moderna. Professor na Universidade São Francisco (Campus Itatiba e Bragança) em disciplinas de TCC, projeto de arquitetura (residencial, institucional, cultural e de lazer), e teoria.",
+	contato_titulo = "COMO NOS ENCONTRAR",
+	contato_subtitulo = "CONTATO",
+	contato_email_titulo = "E-mail",
+	contato_email = "contato@relta.com.br",
+	contato_tel_titulo = "TELEFONES",
+	contato_tel = "(11) 93234-2354 / (11) 2365-8439",
+	form_titulo = "ENVIE UMA MENSAGEM",
+	form_desc = "Deixe seus contatos e uma prévia de sua necessidade que retornaremos o contato.",
+	form_subtitulo = "FORMULÁRIO DE CONTATO",
+	form_nome = "Nome:",
+	form_email = "E-mail:",
+	form_tel = "Telefone:",
+	form_msg = "Mensagem:",
+	form_btn = "Enviar",
+	copyright = "©Copyright <a href='http://www.relta.com.br/'>Relta.com</a> 2014 All rights reserved";
+	
+//Funcao verifica e altera o valor do elemnto HTML
+function validaID (id, valor){
+	var element = !!document.querySelector(id)
+	if(element){
+		document.querySelector(id).innerHTML = valor
+	} else {
+		console.log("O ID "+id+" nao existe!")
+	}
+}
+
+function validaMetaDados (id, nome, desc){
+	var element = !!document.querySelector(id)
+	var el = document.querySelector(id)
+	if(element){
+		el.dataset.title = nome
+		el.dataset.description = desc
+	} else {
+		console.log("O ID "+id+" nao existe!")
+	}
+}
+
+function validaBtn (id, valor){
+	var element = !!document.querySelector(id)
+	var el = document.querySelector(id)
+	if(element){
+		el.value = valor
+	} else {
+		console.log("O ID "+id+" nao existe!")
+	}
+}
+
+//Atribui o valor da variavel no ID (html)
+this.validaID('#banner-1', banner_1);
+this.validaID('#banner-2', banner_2);
+this.validaID('#banner-3', banner_3);
+this.validaID('#banner-msg-1', banner_msg_1);
+this.validaID('#banner-msg-2', banner_msg_2);
+this.validaID('#banner-msg-3', banner_msg_3);
+this.validaID('#servico_titulo', servico_titulo);
+this.validaID('#servico_msg', servico_msg);
+this.validaID('#servico_1_titulo', servico_1_titulo);
+this.validaID('#servico_1_desc', servico_1_desc);
+this.validaID('#servico_2_titulo', servico_2_titulo);
+this.validaID('#servico_2_desc', servico_2_desc);
+this.validaID('#servico_3_titulo', servico_3_titulo);
+this.validaID('#servico_3_desc', servico_3_desc);
+this.validaID('#port_titulo', port_titulo);
+this.validaID('#port_desc', port_desc);
+this.validaID('#port_op_1', port_op_1);
+this.validaID('#port_op_2', port_op_2);
+this.validaID('#port_op_3', port_op_3);
+this.validaID('#port_op_4', port_op_4);
+this.validaID('#port_op_5', port_op_5);
+this.validaID('#port_op_6', port_op_6);
+this.validaID('#port_op_1_desc', port_op_1_desc);
+this.validaID('#port_op_2_desc', port_op_2_desc);
+this.validaID('#port_op_3_desc', port_op_3_desc);
+this.validaID('#port_op_4_desc', port_op_4_desc);
+this.validaID('#port_op_5_desc', port_op_5_desc);
+this.validaID('#port_op_6_desc', port_op_6_desc);
+this.validaID('#port_pergunta', port_pergunta);
+this.validaID('#port_resposta', port_resposta);
+this.validaID('#btn_contato', btn_contato);
+this.validaID('#sobre_titulo', sobre_titulo);
+this.validaID('#sobre_subtitulo', sobre_subtitulo);
+this.validaID('#sobre_msg', sobre_msg);
+this.validaID('#missao_titulo', missao_titulo);
+this.validaID('#missao_desc', missao_desc);
+this.validaID('#visao_titulo', visao_titulo);
+this.validaID('#visao_desc', visao_desc);
+this.validaID('#valores_titulo', valores_titulo);
+this.validaID('#valores_desc', valores_desc);
+this.validaID('#equipe_titulo', equipe_titulo);
+this.validaID('#equipe_desc', equipe_desc);
+this.validaID('#equipe_p1_nome', equipe_p1_nome);
+this.validaID('#equipe_p1_cargo', equipe_p1_cargo);
+this.validaID('#equipe_p2_nome', equipe_p2_nome);
+this.validaID('#equipe_p2_cargo', equipe_p2_cargo);
+this.validaID('#equipe_p3_nome', equipe_p3_nome);
+this.validaID('#equipe_p3_cargo', equipe_p3_cargo);
+this.validaID('#contato_titulo', contato_titulo);
+this.validaID('#contato_subtitulo', contato_subtitulo);
+this.validaID('#contato_email_titulo', contato_email_titulo);
+this.validaID('#contato_email', contato_email);
+this.validaID('#contato_tel_titulo', contato_tel_titulo);
+this.validaID('#contato_tel', contato_tel);
+this.validaID('#form_titulo', form_titulo);
+this.validaID('#form_desc', form_desc);
+this.validaID('#form_subtitulo', form_subtitulo);
+this.validaID('#form_nome', form_nome);
+this.validaID('#form_email', form_email);
+this.validaID('#form_tel', form_tel);
+this.validaID('#form_msg', form_msg);
+this.validaID('#copyright', copyright);
+this.validaBtn('#form_btn', form_btn);
+this.validaMetaDados('#equipe_p1', equipe_p1_nome, equipe_p1_cv);
+this.validaMetaDados('#equipe_p2', equipe_p2_nome, equipe_p2_cv);
+this.validaMetaDados('#equipe_p3', equipe_p3_nome, equipe_p3_cv);
---
+++ lang/EN.js
@@ -0,0 +1,166 @@
+//Variaveis, cada texto tem uma variavel
+var banner_1 = "",
+	banner_msg_1 = "",
+	banner_2 = "",
+	banner_msg_2 = "",
+	banner_3 = "",
+	banner_msg_3 = "",
+	servico_titulo = "SERVICES PROVIDED",
+	servico_msg = "Our professionals are qualified and have experience in the area of ​​renovation and construction, as well as in the area of ​​Legislation, Certifications, Reports and Management, working with the competent organs of municipal to Federal scale, with the commitment to satisfy the needs of our clients and partners. fast and effective way, always thinking of continuous improvement through preventive and corrective actions adjusting all services and processes within the most rigorous standards of inspection and quality.",
+	servico_1_titulo = "TOWN HALL",
+	servico_1_desc = "Regularization;<br>Approval and Execution of Works;<br>Approval and Execution of Reforms;<br>Operating Licenses;<br>Unfolding and Reminding Lot;<br>Accessibility Project;<br>Adaptation to Safety Standards;<br>Executive / New Construction;<br>CADAN;<br>Tanks and pumps;<br>Equipment License;<br>Auto Security Verification (AVS).",
+	servico_2_titulo = "TECHNICAL PROJECTS",
+	servico_2_desc = "Decoration and Interiors;<br>Landscaping;<br>Three-dimensional projects;<br>Small Reform Communication;<br>Report for Condominium (ABNT 16.280);<br>Habitability Report;<br>Feasibility Report;<br>As Built.",
+	servico_3_titulo = "FIRE DEPARTMENT",
+	servico_3_desc = "Adaptation to Fire Department Rules;<br>Equipment installation;<br>Pressurization of stairs;<br>Initial AVCB;<br>Renovation;<br>Pre Survey;<br>FAT.",
+	port_titulo = "PORTIFOLIO",
+	port_desc = "know all our projects",
+	port_op_1 = "Casa GP",
+	port_op_2 = "Casa Verde",
+	port_op_3 = "CCSP",
+	port_op_4 = "Guarulhos",
+	port_op_5 = "Vl. Madalena",
+	port_op_6 = "",
+	port_op_1_desc = "Descricao da Obra.",
+	port_op_2_desc = "Descricao da Obra.",
+	port_op_3_desc = "Descricao da Obra.",
+	port_op_4_desc = "Descricao da Obra.",
+	port_op_5_desc = "Descricao da Obra.",
+	port_op_6_desc = "Descricao da Obra.",
+	port_pergunta = "DID YOU LIKE OUR PORTIFOLIO?",
+	port_resposta = "Contact us now and we will be happy to serve you.",
+	btn_contato = "Contact Us",
+	sobre_titulo = "WHO WE ARE",
+	sobre_subtitulo = "ABOUT US",
+	sobre_msg = "Founded by Felipe Alves and Raquel Freitas, Relta Arquitutra develops projects in dialogue with nature and in contact with the right intentions, targeting all the needs of its clients. Felipe and Raquel have been working together since 2014, but only in 2016 do they decide to found the corporate society.",
+	missao_titulo = "MISSION",
+	missao_desc = "Provide sustainable environmental solutions to its customers, partners and society, seeking balanced and conscious environmental development.",
+	visao_titulo = "VISION",
+	visao_desc = "Be a reference company in consulting solutions, projects and constructions seeking to operate in the market in a transparent and ethical manner.",
+	valores_titulo = "VALUES",
+	valores_desc = "Commitment, Focus, Objectivity, Practicality, Sustainability, Ethics, Punctuality, Transparency, Quality and Innovation.",
+	equipe_titulo = "THE TEAM",
+	equipe_desc = "The office bets on a close relationship with nature, in favor of the concept of true sustainability. Realized projects that work with ecological materials, making wide use of elements such as brightness, transparency, the fluidity of spaces and structural lightness. Through this proposal, establishing a concept of Brazilian architecture, with the reappropriation of modernism in dialogue with the environment.",
+	equipe_p1_nome = "Felipe Alves",
+	equipe_p1_cargo = "Founder & CEO",
+	equipe_p1_cv = "He joined the market in 2010 as an architecture intern while doing freelance projects. In 2011 he worked with architect Roberto Montenegro where he gained experience with decorations and renovations, later in 2012 he joined the company Atthelie da Arquitetura, where he gained experience in Approvals, Cetesb, Covisa, Fire Department, Sanitary Surveillance, Monitoring. and Construction Management. In 2014, he opened the company as an individual entrepreneur, moving to the market on his own.",
+	equipe_p2_nome = "Raquel Toni",
+	equipe_p2_cargo = "Co-Founder and architect",
+	equipe_p2_cv = "Architect since 2016, worked as a trainee at Hospital Israelita Albert Einstein in 2012 doing interior design projects, later in 2013 joined as a trainee at Atthelie da Arquitetura, performing process and project analysis with City Halls. Provided services in 2015 to the company PW Design - Arquitetura e Interiores, performing the development of residential and commercial projects, as well as the definitions of materials and presentation of projects on site. In addition to having started his profession, with two years of participation in the City of São Paulo, for tax registration and zoning of the city.",
+	equipe_p3_nome = "Beto Montenegro",
+	equipe_p3_cargo = "Architect",
+	equipe_p3_cv = "Acting architect with experience in architectural design. Graduated from the Faculty of Architecture and Urbanism of the University of São Paulo (FAUUSP, 1998), Master (2007) and Doctor (2012) by the same Institution in History of Architecture and Urbanism, with research developed on prefabrication in modern architecture. Professor at the São Francisco University (Itatiba and Bragança Campus) in CBT, architecture project (residential, institutional, cultural and leisure), and theory.",
+	contato_titulo = "HOW TO FIND US",
+	contato_subtitulo = "CONTACT",
+	contato_email_titulo = "E-mail",
+	contato_email = "contato@relta.com.br",
+	contato_tel_titulo = "PHONES",
+	contato_tel = "(11) 93234-2354 / (11) 2365-8439",
+	form_titulo = "SEND A MESSAGE",
+	form_desc = "Leave your contacts and a preview of your need we will return the contact.",
+	form_subtitulo = "CONTACT FORM",
+	form_nome = "Name:",
+	form_email = "E-mail:",
+	form_tel = "Phone:",
+	form_msg = "Message:",
+	form_btn = "Submit",
+	copyright = "©Copyright <a href='http://www.relta.com.br/'>Relta.com</a> 2014 All rights reserved";
+	
+//Funcao verifica e altera o valor do elemnto HTML
+function validaID (id, valor){
+	var element = !!document.querySelector(id)
+	if(element){
+		document.querySelector(id).innerHTML = valor
+	} else {
+		console.log("O ID "+id+" nao existe!")
+	}
+}
+
+function validaMetaDados (id, nome, desc){
+	var element = !!document.querySelector(id)
+	var el = document.querySelector(id)
+	if(element){
+		el.dataset.title = nome
+		el.dataset.description = desc
+	} else {
+		console.log("O ID "+id+" nao existe!")
+	}
+}
+
+function validaBtn (id, valor){
+	var element = !!document.querySelector(id)
+	var el = document.querySelector(id)
+	if(element){
+		el.value = valor
+	} else {
+		console.log("O ID "+id+" nao existe!")
+	}
+}
+
+//Atribui o valor da variavel no ID (html)
+this.validaID('#banner-1', banner_1);
+this.validaID('#banner-2', banner_2);
+this.validaID('#banner-3', banner_3);
+this.validaID('#banner-msg-1', banner_msg_1);
+this.validaID('#banner-msg-2', banner_msg_2);
+this.validaID('#banner-msg-3', banner_msg_3);
+this.validaID('#servico_titulo', servico_titulo);
+this.validaID('#servico_msg', servico_msg);
+this.validaID('#servico_1_titulo', servico_1_titulo);
+this.validaID('#servico_1_desc', servico_1_desc);
+this.validaID('#servico_2_titulo', servico_2_titulo);
+this.validaID('#servico_2_desc', servico_2_desc);
+this.validaID('#servico_3_titulo', servico_3_titulo);
+this.validaID('#servico_3_desc', servico_3_desc);
+this.validaID('#port_titulo', port_titulo);
+this.validaID('#port_desc', port_desc);
+this.validaID('#port_op_1', port_op_1);
+this.validaID('#port_op_2', port_op_2);
+this.validaID('#port_op_3', port_op_3);
+this.validaID('#port_op_4', port_op_4);
+this.validaID('#port_op_5', port_op_5);
+this.validaID('#port_op_6', port_op_6);
+this.validaID('#port_op_1_desc', port_op_1_desc);
+this.validaID('#port_op_2_desc', port_op_2_desc);
+this.validaID('#port_op_3_desc', port_op_3_desc);
+this.validaID('#port_op_4_desc', port_op_4_desc);
+this.validaID('#port_op_5_desc', port_op_5_desc);
+this.validaID('#port_op_6_desc', port_op_6_desc);
+this.validaID('#port_pergunta', port_pergunta);
+this.validaID('#port_resposta', port_resposta);
+this.validaID('#btn_contato', btn_contato);
+this.validaID('#sobre_titulo', sobre_titulo);
+this.validaID('#sobre_subtitulo', sobre_subtitulo);
+this.validaID('#sobre_msg', sobre_msg);
+this.validaID('#missao_titulo', missao_titulo);
+this.validaID('#missao_desc', missao_desc);
+this.validaID('#visao_titulo', visao_titulo);
+this.validaID('#visao_desc', visao_desc);
+this.validaID('#valores_titulo', valores_titulo);
+this.validaID('#valores_desc', valores_desc);
+this.validaID('#equipe_titulo', equipe_titulo);
+this.validaID('#equipe_desc', equipe_desc);
+this.validaID('#equipe_p1_nome', equipe_p1_nome);
+this.validaID('#equipe_p1_cargo', equipe_p1_cargo);
+this.validaID('#equipe_p2_nome', equipe_p2_nome);
+this.validaID('#equipe_p2_cargo', equipe_p2_cargo);
+this.validaID('#equipe_p3_nome', equipe_p3_nome);
+this.validaID('#equipe_p3_cargo', equipe_p3_cargo);
+this.validaID('#contato_titulo', contato_titulo);
+this.validaID('#contato_subtitulo', contato_subtitulo);
+this.validaID('#contato_email_titulo', contato_email_titulo);
+this.validaID('#contato_email', contato_email);
+this.validaID('#contato_tel_titulo', contato_tel_titulo);
+this.validaID('#contato_tel', contato_tel);
+this.validaID('#form_titulo', form_titulo);
+this.validaID('#form_desc', form_desc);
+this.validaID('#form_subtitulo', form_subtitulo);
+this.validaID('#form_nome', form_nome);
+this.validaID('#form_email', form_email);
+this.validaID('#form_tel', form_tel);
+this.validaID('#form_msg', form_msg);
+this.validaBtn('#form_btn', form_btn);
+this.validaID('#copyright', copyright);
+this.validaMetaDados('#equipe_p1', equipe_p1_nome, equipe_p1_cv);
+this.validaMetaDados('#equipe_p2', equipe_p2_nome, equipe_p2_cv);
+this.validaMetaDados('#equipe_p3', equipe_p3_nome, equipe_p3_cv);
